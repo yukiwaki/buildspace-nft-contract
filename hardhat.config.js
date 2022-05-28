505b1b644dab10f2ef0c830b4a93aa0858658247
yukiwaki <yuki@pont.co>
2022-05-28 10:09:15 +0900

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: '0.8.1',
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
